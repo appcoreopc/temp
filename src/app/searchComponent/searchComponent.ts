e8b729c891647071052db0efa47b3d68a3c160c0
jeremy <kepung@gmail.com>
2017-04-14 15:19:43 +1200

    fake spec

--- src/app/searchComponent/searchComponent.ts
+++ src/app/searchComponent/searchComponent.ts
@@ -5,13 +5,25 @@ import { FormControl } from '@angular/forms';
   selector: 'my-app',
   templateUrl: `./searchComponent.html`
 })
-export class SearchComponent { 
+export class SearchComponent {
 
-  name = 'Angular'; 
+  name = 'Angular';
+
   firstname = new FormControl();
   lastname = new FormControl();
 
   constructor() {
+
+    //search.set('action', 'opensearch');
+    //search.set('search', term);
+    //search.set('format', 'json');
+
+    //this.term.valueChanges
+    //           .debounceTime(400)        // wait for 400ms pause in events
+    //            .distinctUntilChanged()   // ignore if next search term is same as previous
+    //           .subscribe(term => this.wikipediaService.search(term).then(items => this.items = items));
+
     //this.firstname.valueChanges
+
   }
 }
