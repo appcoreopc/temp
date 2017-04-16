bc7bd78eff492cb0403ac3acb6039bfdc2137f7f
jeremy <kepung@gmail.com>
2017-04-16 18:33:30 +1200

    added unit test and configure karma.config.js
    browser-test-shim.js
    systemjs.config.js
    systemjs-angular-loader.js
    systemjs.config.extras.js

---
+++ src/app/addPersonComponent/addPersonComponent.spec.ts
@@ -0,0 +1,46 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { AddPersonComponent } from './addPersonComponent';
+import { PersonService } from '../services/PersonService'
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+describe('AddPersonComponent (templateUrl)', () => {
+
+  let comp: AddPersonComponent;
+  let fixture: ComponentFixture<AddPersonComponent>;
+  let de: DebugElement;
+  let el: HTMLElement;
+
+  // async beforeEach
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpModule, ReactiveFormsModule, CommonModule, FormsModule
+      ],
+      declarations: [AddPersonComponent], // declare the test component, 
+      providers: [PersonService],
+    }).compileComponents();  // compile template and css
+  }));
+
+  // synchronous beforeEach
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AddPersonComponent);
+
+    comp = fixture.componentInstance; // BannerComponent test instance
+
+    // query for the title <h1> by CSS element selector
+    //de = fixture.debugElement.query(By.css('h1'));
+    //el = de.nativeElement;
+  });
+
+  it('no title in the DOM until manually call `detectChanges`', () => {
+    fixture.detectChanges();
+    //expect('').toEqual('');
+  });
+
+});
--- src/app/addPersonComponent/addPersonComponent.ts
+++ src/app/addPersonComponent/addPersonComponent.ts
@@ -10,21 +10,19 @@ export class AddPersonComponent {
 
   person : Person = new Person(); 
 
-  constructor(private personService: PersonService) {
-
-     
-  }
+  // constructor(private personService: PersonService) {
+  // }
 
   addPerson(): void {
-    console.log('test');
-    var p: Person = {
-      firstName: '', lastName: "", age: 12
-    }
-    this.personService.addPerson(p);
+    // console.log('test');
+    // var p: Person = {
+    //   firstName: '', lastName: "", age: 12
+    // }
+    // this.personService.addPerson(p);
   }
 
   onSubmit(person: Person): void {
-    console.log('test submit add person');
+    //console.log('test submit add person');
   }
 }
 
