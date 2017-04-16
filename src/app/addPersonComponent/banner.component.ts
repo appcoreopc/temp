6b1408f64912da1396c583abd467c92bd4242d9a
jeremy <kepung@gmail.com>
2017-04-16 19:12:37 +1200

    removed unused components

--- src/app/addPersonComponent/banner.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-banner',
-  templateUrl: './banner.component.html'
-})
-export class BannerComponent {
-  title = 'Test Tour of Heroes';
-}
